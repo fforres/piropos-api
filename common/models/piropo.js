921d2a89a95c971c76b7d1b98b564dc410f6b550
felipe torres koding <felipe.torressepulveda@gmail.com>
2014-12-28 14:21:30 +0000

    update modelo piropos

--- common/models/piropo.js
+++ common/models/piropo.js
@@ -1,19 +1,42 @@
 module.exports = function(Piropo) {
 	
+	Piropo.searchIgnoreCase = function(searchParameter, cb) {
+        var pattern = new RegExp(searchParameter+"$", "i"); /* case-insensitive RegExp search */ 
+        ob = {
+                order:"votoAFavor DESC",
+                limit:10,
+                where:
+                {
+                    textoUpperCase:
+                    {
+                        like: searchParameter.toUpperCase()
+                    }
+                }
+        };
+        console.log(ob)
+		Piropo.find(ob,function(err,piropos){
+		    if(err){
+		        cb(null,[]);    
+		    }else{
+                cb(null,piropos);
+		    }
+		});
+	};
+	
 	Piropo.prototype.addVotoAFavor = function(cb) {
 		this.votoAFavor = this.votoAFavor + 1;
-		this.votoTotal = this.votoAFavor - this.votoEnContra
+		this.votoTotal = this.votoAFavor - this.votoEnContra;
 		this.updateAttributes(this,function(err,data){
 			cb(null,data);
-		})
+		});
 	};
 
 	Piropo.prototype.addVotoEnContra = function(cb) {
 		this.votoEnContra = this.votoEnContra + 1;
-		this.votoTotal = this.votoAFavor - this.votoEnContra
+		this.votoTotal = this.votoAFavor - this.votoEnContra;
 		this.updateAttributes(this,function(err,data){
 			cb(null,data);
-		})
+		});
 	};
 	
 	Piropo.remoteMethod('addVotoAFavor', {
@@ -31,14 +54,28 @@ module.exports = function(Piropo) {
 			arg: 'msg',
 			type: 'string' 
 		}
-	});   
+	});  
+	
+	Piropo.remoteMethod('searchIgnoreCase', {
+		isStatic: true,
+        http: {verb: 'get'},
+		accepts: {
+            arg:'searchParameter',
+            type:'string'
+		},
+		returns: {
+			arg: 'piropos',
+			type: 'string'
+		}
+	}); 
 	
 	Piropo.beforeRemote('create', function(ctx, piropo, next) {
 		ctx.args.data.creado = new Date();
 		ctx.args.data.votoAFavor = ctx.args.data.votoEnContra = ctx.args.data.votoTotal=0;
 		var texto = ctx.args.data.texto.trim().replace("\n", "").split(" ").join("-");
-		ctx.args.data.seoencoded = encodeURIComponent(texto).replace(/'/g,"%27").replace(/"/g,"%22")
-		ctx.args.data.seo = texto
+		ctx.args.data.seoencoded = encodeURIComponent(texto).replace(/'/g,"%27").replace(/"/g,"%22");
+		ctx.args.data.seo = texto;
+		ctx.args.data.textoUpperCase = texto.toUpperCase();
 		next();
 	});
 	
